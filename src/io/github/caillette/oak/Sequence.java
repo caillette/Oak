61700b270f60603f99eeead263393506ac89d85b
caillette <laurent.caillette@gmail.com>
2013-10-20 10:34:48 +0200

    Sequence enhancements.

--- src/io/github/caillette/oak/Sequence.java
+++ src/io/github/caillette/oak/Sequence.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 
 /**
- * Kotlin-friendly immutable indexed collection.
+ * Kotlin-aware immutable indexed collection.
  *
  * @author Laurent Caillette
  */
@@ -32,8 +32,7 @@ public final class Sequence< ELEMENT > {
         elements.length == 0 ? IntRange.EMPTY : new IntRange( 0, elements.length - 1 )
     ) ;
     for ( int i = 0 ; i < elements.length ; i++ ) {
-      final ELEMENT element = elements[ i ] ;
-      if ( element == null ) {
+      if ( elements[ i ] == null ) {
         throw new IllegalArgumentException( "Null element at [ " + i  + "]" ) ;
       }
     }
@@ -50,6 +49,29 @@ public final class Sequence< ELEMENT > {
     newElements[ elements.length ] = appended ;
     final IntRange newRange = new IntRange( 0, elements.length ) ;
     return new Sequence<>( newElements, newRange ) ;
+  }
+
+  static boolean isEmpty( @NotNull final IntRange intRange ) {
+    return intRange.getEnd() > intRange.getStart() ;
+  }
+
+  static int size( @NotNull final IntRange intRange ) {
+    return isEmpty( intRange ) ? 0 : intRange.getEnd() - intRange.getStart() + 1 ;
+  }
+
+  @NotNull
+  public Sequence< ELEMENT > plus( @NotNull final Sequence< ELEMENT > appended ) {
+    final int thisSize = size( indices ) ;
+    final int appendedSize = size( appended.indices ) ;
+    final int newSize = thisSize + appendedSize ;
+    if( newSize == 0 ) {
+      return new Sequence<>( Arrays.copyOf( elements, 0 ), IntRange.EMPTY ) ;
+    } else {
+      final ELEMENT[] newElements =  Arrays.copyOf( elements, newSize ) ;
+      System.arraycopy( appended.elements, 0, newElements, thisSize, appendedSize ) ;
+      final IntRange newRange = new IntRange( 0, newSize - 1 ) ;
+      return new Sequence<>( newElements, newRange ) ;
+    }
   }
 
 }
